436d817078be90b0cef922e73ac78ca46066f184
unknown <erlangganiko42@gmail.com>
2026-02-02 17:38:44 +0700

    update web route

--- routes/web.php
+++ routes/web.php
@@ -3,5 +3,25 @@
 use Illuminate\Support\Facades\Route;
 
 Route::get('/', function () {
-    return view('welcome');
+    return view('index');
+});
+
+
+Route::get('/dashboard', function () {
+    return view('dashboard.index');
+});
+
+Route::get('/dashboard/products', function () {
+    return view('dashboard.products.index');
+});
+
+Route::get('/dashboard/products/detail', function () {
+    return view('dashboard.products.show');
+});
+Route::get('/dashboard/inventories', function () {
+    return view('dashboard.inventory.index');
+});
+
+Route::get('/dashboard/categories', function () {
+    return view('dashboard.category.index');
 });
